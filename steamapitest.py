6552b4d2610e79fbbec35f918e10f4d31b9c6711
MattMohaupt <mmohaupt123@gmail.com>
2024-11-05 19:40:21 -0600

    update lowerbounded

--- steamapitest.py
+++ steamapitest.py
@@ -14,10 +14,6 @@ response = steam.users.search_user("mmohaupt")
 medf = pd.DataFrame.from_dict(response)
 #76561198284996299     mmohaupt steamid
 #76561197960298195     not real steam id
-gamer = steam.users.get_owned_games(<PUT STEAM USER ID HERE>)
+gamer = steam.users.get_owned_games(76561198284996299)
 gamerdf = pd.DataFrame.from_dict(gamer)
-# print(medf)
-if(not gamerdf.empty):
-    print(gamerdf['game_count'][0])
-else:
-    print("None")
+print(gamerdf)
